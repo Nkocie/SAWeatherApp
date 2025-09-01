7bba457f1145732ea93a36e6007c0ffcfd6e5c9e
Nkosiyezwa M Mpanza <Nkociempanza121@gmail.com>
2025-09-01 15:46:51 +0200

    Update weather.py

--- weather.py
+++ weather.py
@@ -25,4 +25,4 @@ if response.status_code == 200 and "main" in data:
     print("Condition:", data["weather"][0]["description"])
 else:
     print("Error:", data.get("message", "Unable to fetch weather data"))
-
+    
